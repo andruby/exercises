d21f001bc605a89e4b3966624806c5b023c522a5
Andrew Fecheyr <andrew@bedesign.be>
2014-05-04 09:47:02 +0200

    Ruby version of first 10k primes

---
+++ first_primes/first_primes.rb
@@ -0,0 +1,8 @@
+# takes 4.8s with ruby 2.1.0p0
+primes = []
+x = 1
+begin
+  x += 1
+  primes << x unless primes.any? { |prime| x % prime == 0 }
+end until primes.length == 10000
+puts primes.join(' ')
