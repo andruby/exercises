8ce2b87f83b51fe4f98c0f6725d3ea7fde5b4229
Andrew Fecheyr <andrew@bedesign.be>
2015-02-01 22:13:31 +0100

    Use a vector list in rust for nicer code

--- first_primes/first_primes.rs
+++ first_primes/first_primes.rs
@@ -1,21 +1,21 @@
 // 0.90s on rust 1.0.0-alpha
 fn main() {
-  let mut primes: [u32; 10000] = [0; 10000];
-  let mut x = 2;
-  let mut len = 0;
+  let mut primes = vec![];
+  let mut x: u32 = 2;
+  let mut len: u32 = 0;
   while len < 10000 {
     let mut is_prime = true;
-    for i in range(0, len) {
-      if x % primes[i] == 0 {
+    for &prime in primes.iter() {
+      if x % prime == 0 {
         is_prime = false;
         break;
       }
     }
     if is_prime {
-      primes[len] = x;
+      primes.push(x);
       len = len + 1;
     }
-    x = x+1;
+    x = x + 1;
   }
   for &v in primes.iter() {
     print!("{} ", v);
